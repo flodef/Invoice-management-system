2882087c2f8a8711b704288a43f4d0539dd9f7fb
Flojito <flojito@archireport.com>
2025-07-11 03:50:19 +0200

    refactor: remove service worker registration and PWA functionality

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import { ConvexAuthProvider } from "@convex-dev/auth/react";
 import { ConvexReactClient } from "convex/react";
 import "./index.css";
 import App from "./App";
-import { registerServiceWorker } from "./registerServiceWorker";
 
 const convex = new ConvexReactClient(import.meta.env.VITE_CONVEX_URL as string);
 
@@ -12,6 +11,3 @@ createRoot(document.getElementById("root")!).render(
     <App />
   </ConvexAuthProvider>,
 );
-
-// Register service worker for PWA functionality
-registerServiceWorker();
